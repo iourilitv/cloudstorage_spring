a434d6a2cb423ae6f2b6ca976a9bb7e1571b7ebc
iourilitv <iourilitv1965@gmail.com>
2020-04-05 16:11:27 +0300

    Rearranged springframework dependencies for Beans in pom.xml(project). Created ServerSpringConfig class and changed application starting method to the method "by Bean using". Server app works correctly.

--- server/src/main/java/ServerMain.java
+++ server/src/main/java/ServerMain.java
@@ -1,12 +1,23 @@
 import control.CloudStorageServer;
+import control.ServerSpringConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * The main class of server cloudstorage applet.
  */
 public class ServerMain {
 
+//    public static void main(String[] args) throws Exception {
+//        CloudStorageServer css = new CloudStorageServer();
+//        css.initConfiguration();
+//        css.run();
+//    }
     public static void main(String[] args) throws Exception {
-        CloudStorageServer css = new CloudStorageServer();
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(ServerSpringConfig.class);
+
+        CloudStorageServer css = context.getBean("cloudStorageServer",
+                CloudStorageServer.class);
         css.initConfiguration();
         css.run();
     }
